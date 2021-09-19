3daf45f42f365916f13a353aa4e76e03d7889f2c
Alex Ilushyk <ailushyk@gmail.com>
2021-09-19 18:05:15 +0200

    setup code quality

--- src/cli/commands/welcome.ts
+++ src/cli/commands/welcome.ts
@@ -4,12 +4,11 @@ type Options = {
   name: string;
 };
 
-export const command: string = 'welcome <name>';
-export const desc: string = 'Your own mf space';
+export const command = 'welcome <name>';
+export const desc = 'Your own mf space';
 
 export const builder: CommandBuilder<Options, Options> = (yargs) =>
-  yargs
-    .positional('name', { type: 'string', demandOption: true });
+  yargs.positional('name', { type: 'string', demandOption: true });
 
 export const handler = (argv: Arguments<Options>): void => {
   const { name, upper } = argv;
--- src/cli/index.ts
+++ src/cli/index.ts
@@ -2,11 +2,7 @@
 import yargs from 'yargs/yargs';
 import { hideBin } from 'yargs/helpers';
 
-yargs(hideBin(process.argv))
-  .commandDir('commands')
-  .strict()
-  .alias({
-    v: 'version',
-    h: 'help'
-  })
-  .argv;
+yargs(hideBin(process.argv)).commandDir('commands').strict().alias({
+  v: 'version',
+  h: 'help',
+}).argv;
