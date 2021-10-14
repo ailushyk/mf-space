e537bd35a92377928e81a1c8b5c1b46d95c4b2b3
Alex Ilushyk <ailushyk@gmail.com>
2021-10-14 23:52:53 +0200

    create space (#2)
    * refactoring bin
    
    * install degit
    
    * add cli command: create
    
    * create space

---
+++ src/cli/commands/create/spaceTemplates.ts
@@ -0,0 +1,15 @@
+export const spaceTemplates = {
+  // app: {
+  //   default: 'ailushyk/mf-space/src/templates/space',
+  //   react: 'ailushyk/mf-space/src/templates/space',
+  // },
+  // package: {
+  //   default: 'ailushyk/mf-space/src/templates/space',
+  //   react: 'ailushyk/mf-space/src/templates/space',
+  // },
+  // shell: {
+  //   default: 'ailushyk/mf-space/src/templates/space',
+  //   react: 'ailushyk/mf-space/src/templates/shell/react',
+  // },
+  space: 'ailushyk/mf-space/src/templates/space',
+};
